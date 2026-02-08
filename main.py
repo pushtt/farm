5ea7b8cde447b6c072553d8f038ec5f9659ff490
Thinh Tran <thinhphuctran@gmail.com>
2026-02-08 22:03:25 +0100

    remove everything

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from farm!")
-
-
-if __name__ == "__main__":
-    main()
--- routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cars import router as cars_router
-from .users import router as users_router
-
-__all__ = ["cars_router", "users_router"]
